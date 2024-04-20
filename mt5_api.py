60bd952efe0a8e7988b9cfb84707b7ad8ff53cbb
Trader-IKU <x1o9o0x@gmail.com>
2024-04-20 11:02:26 +0900

    update

--- mt5_api.py
+++ mt5_api.py
@@ -1,53 +1,54 @@
+import os
+import sys
+sys.path.append('../Libraries/trade')
 
 import MetaTrader5 as mt5
 import pandas as pd
 from dateutil import tz
-from datetime import datetime, timedelta
-
-
+from datetime import datetime, timedelta, timezone
+from time_utils import TimeUtils
 JST = tz.gettz('Asia/Tokyo')
 UTC = tz.gettz('utc')  
         
-def now():
-    t = datetime.now(tz=UTC)
-    return t
+def server_time(begin_month, begin_sunday, end_month, end_sunday, delta_hour_from_gmt_in_summer):
+    now = datetime.now(JST)
+    dt, tz = TimeUtils.delta_hour_from_gmt(now, begin_month, begin_sunday, end_month, end_sunday, delta_hour_from_gmt_in_summer)
+    #delta_hour_from_gmt  = dt
+    #server_timezone = tz
+    #print('SeverTime GMT+', dt, tz)
+    return dt, tz  
+  
+def adjust_summer_time(time: datetime):
+    dt, tz = server_time(3, 2, 11, 1, 3.0)
+    return time - dt
 
-# numpy timestamp -> pydatetime naive
-def nptimestamp2pydatetime(npdatetime):
-    timestamp = (npdatetime - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
-    dt = datetime.utcfromtimestamp(timestamp)
-    return dt
-
-def slice(df, ibegin, iend):
-    new_df = df.iloc[ibegin: iend + 1, :]
-    return new_df
-
-def df2dic(df: pd.DataFrame):
-    dic = {}
-    for column in df.columns:
-        dic[column] = df[column].to_numpy()
-    return dic
-
-def time_str_2_datetime(df, time_column, format='%Y-%m-%d %H:%M:%S'):
-    time = df[time_column].to_numpy()
-    new_time = [datetime.strptime(t, format) for t in time]
-    df[time_column] = new_time
-        
+def adjust(dic, column='time'):
+    utc = dic[column]
+    time = []
+    jst = []
+    for ts in utc:
+        t0 = pd.to_datetime(ts)
+        t0 = t.replace(tzinfo=UTC)
+        t = adjust_summer_time(t0)
+        time.append(t)
+        tj = t.astimezone(JST)
+        jst.append(tj)  
+    dic[column] = time
+    dic['jst'] = jst
+            
 class Mt5Api:
-    def __init__(self, symbol):
-        self.symbol = symbol
-        self.ticket = None
+    def __init__(self):
+        self.connect()
         
-    @staticmethod
     def connect():
         if mt5.initialize():
             print('Connected to MT5 Version', mt5.version())
         else:
             print('initialize() failed, error code = ', mt5.last_error())
 
-    def get_rates(self, timeframe: str, length: int):
-        #print(self.symbol, timeframe)
-        rates = mt5.copy_rates_from_pos(self.symbol,  timeframe, 0, length)
+    def get_rates(self, symbol: str, timeframe: str, length: int):
+        #print(symbol, timeframe)
+        rates = mt5.copy_rates_from_pos(symbol,  timeframe, 0, length)
         if rates is None:
             raise Exception('get_rates error')
         return self.parse_rates(rates)
@@ -55,6 +56,8 @@ class Mt5Api:
     def parse_rates(self, rates):
         df = pd.DataFrame(rates)
         df['time'] = pd.to_datetime(df['time'], unit='s')
+        adjust(df, 'time')
+        
         return df
         
 
